1c24159b2069053faf89bad67efa059ff38beb8c
taem0001 <s235268@dtu.dk>
2024-08-13 11:51:12 +0200

    Added game loop in GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,57 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
+    // screen size variables
     private final int SCREENWIDTH = 640;
     private final int SCREENHEIGHT = 480;
+
+    // game running variables
+    private boolean running = false;
+    private int FPS = 60;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(SCREENWIDTH, SCREENHEIGHT));
         this.setDoubleBuffered(true);
+    }
+
+    public synchronized void start() {
+        new Thread(this).start();
+        running = true;
+    }
+
+    public synchronized void stop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            double drawInterval = 1000000000 / FPS;
+            double nextDrawTime = System.nanoTime() + drawInterval;
+
+            update();
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000;
+                remainingTime = remainingTime < 0 ? 0 : remainingTime;
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
